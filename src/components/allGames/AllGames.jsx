da87abd216794af5c2b837ade68c942f801acd26
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-04-27 12:15:41 -0300

    feat: loader pacman

--- src/components/allGames/AllGames.jsx
+++ src/components/allGames/AllGames.jsx
@@ -3,13 +3,16 @@ import useFetch from "../../hooks/useFetch";
 import Search from "../../utils/Search";
 import GameCard from "../gameCard/GameCard";
 import logo from "/logo/keep-gaming.png";
+import Loader from "react-loaders";
+
+import "loaders.css/loaders.css";
 
 const gamesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const AllGames = () => {
   const [games, setGames] = useState([]);
-  const { request, loading, setLoading } = useFetch();
+  const { request, loading } = useFetch();
 
   useEffect(() => {
     const getGames = async () => {
@@ -25,16 +28,26 @@ const AllGames = () => {
 
   return (
     <section className="px-5 mt-20 lg:px-0">
-      <div className="container">
-        <div className="flex gap-5 items-center">
-          <img src={logo} className="w-[100px]" alt="" />
-          <Search />
-        </div>
-        <h1 className="text-7xl mt-10 mb-5">Todos os Jogos</h1>
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 items-center">
-          {games.map((game, index) => (
-            <GameCard game={game} key={index} showLink={true} />
-          ))}
+      <div className="flex gap-5 items-center">
+        <div className="container">
+          <div className="flex gap-5">
+            <img src={logo} className="w-[100px]" alt="" />
+            <Search />
+          </div>
+          {loading ? (
+            <div className="loader-active flex mt-20 justify-center items-center">
+              <Loader type="pacman" />
+            </div>
+          ) : (
+            <>
+              <h1 className="text-7xl mt-10 mb-5">Todos os Jogos</h1>
+              <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 items-center">
+                {games.map((game, index) => (
+                  <GameCard game={game} key={index} showLink={true} />
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </section>
