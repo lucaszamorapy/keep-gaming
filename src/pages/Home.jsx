6ba1ff1b92ebf5d4d38a676462348b9e1b2adb3b
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-04-29 22:41:25 -0300

    fix: banner router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import AllGames from "../components/AllGames";
+import Banner from "../components/banner/Banner";
 
 const Home = () => {
   return (
     <div>
+      <Banner />
       <AllGames />
     </div>
   );
