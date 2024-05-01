fafcd73a2f509f741580c20d3bed9b413f25ba15
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-05-01 13:43:08 -0300

    feat: scroll to top on page change

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import GamePage from "./pages/GamePage";
 import Header from "./components/header/Header";
 import SearchPage from "./pages/SearchPage";
 import Footer from "./components/Footer";
+import ScrollToTopOnPageChange from "./components/ScrollToTopOnPageChange";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <ScrollToTopOnPageChange />
         <main className="AppBody">
           <Routes>
             <Route path="/" element={<Home />} />
